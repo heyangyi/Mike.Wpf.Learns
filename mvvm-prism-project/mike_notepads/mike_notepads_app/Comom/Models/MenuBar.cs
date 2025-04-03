c9ea6680b80f30b2b5ac79abcbadbd97ccb6ea20
yangyi <svip.hyy@qq.com>
2025-04-03 18:25:25 +0800

    左侧菜单栏

---
+++ mvvm-prism-project/mike_notepads/mike_notepads_app/Comom/Models/MenuBar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mike_notepads_app.Comom.Models
+{
+	/// <summary>
+	/// 系统导航菜单实体类
+	/// </summary>
+    public class MenuBar:BindableBase
+    {
+		private string name;
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private string icon;
+		/// <summary>
+		/// 图标
+		/// </summary>
+		public string Icon
+		{
+			get { return icon; }
+			set { icon = value; }
+		}
+
+		private string nameSpace;
+        /// <summary>
+        /// 菜单命名空间
+        /// </summary>
+        public string NameSpace
+		{
+			get { return nameSpace; }
+			set { nameSpace = value; }
+		}
+	}
+}
--- mvvm-prism-project/mike_notepads/mike_notepads_app/ViewModels/MainWindowModel.cs
+++ mvvm-prism-project/mike_notepads/mike_notepads_app/ViewModels/MainWindowModel.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using mike_notepads_app.Comom.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace mike_notepads_app.ViewModels
 {
     public class MainWindowModel : BindableBase
     {
+        public MainWindowModel()
+        {
+            MenuBars = new ObservableCollection<MenuBar>();
+            CreateMenuBar();
+        }
 
+        private ObservableCollection<MenuBar> menuBars;
+
+        public ObservableCollection<MenuBar> MenuBars
+        {
+            get { return menuBars; }
+            set { menuBars = value; RaisePropertyChanged(); }
+        }
+
+        private void CreateMenuBar()
+        {
+            MenuBars.Add(new MenuBar() { Icon = "HomePlus", Name = "首页", NameSpace = "HomeView" });
+            MenuBars.Add(new MenuBar() { Icon = "ReceiptTextCheckOutline", Name = "待办事项", NameSpace = "ToDoView" });
+            MenuBars.Add(new MenuBar() { Icon = "BookmarkMultiple", Name = "备忘录", NameSpace = "MemoView" });
+            MenuBars.Add(new MenuBar() { Icon = "CogTransferOutline", Name = "设置", NameSpace = "SettingView" });
+        }
     }
 }
