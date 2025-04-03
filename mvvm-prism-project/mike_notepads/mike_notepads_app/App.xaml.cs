5509b1ed866a4d68e3b10ecc67254ed90a725de5
yangyi <svip.hyy@qq.com>
2025-04-03 11:30:58 +0800

    集成Prism框架

--- mvvm-prism-project/mike_notepads/mike_notepads_app/App.xaml.cs
+++ mvvm-prism-project/mike_notepads/mike_notepads_app/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using mike_notepads_app.ViewModels;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -7,7 +8,17 @@ namespace mike_notepads_app;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : PrismApplication
 {
+    protected override Window CreateShell()
+    {
+        // 返回一个窗体，从容器中解析
+        return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.RegisterForNavigation<MainWindow, MainWindowModel>();
+    }
 }
 
