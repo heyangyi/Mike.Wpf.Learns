6af0f4a34a3284fc8558f4d74af91db09dc38fb2
yangyi <svip.hyy@qq.com>
2025-04-14 18:31:43 +0800

    fix

--- mvvm-prism-project/mike_notepads/mike_notepads_app/ViewModels/SettingView/PersonalizeViewModel.cs
+++ mvvm-prism-project/mike_notepads/mike_notepads_app/ViewModels/SettingView/PersonalizeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignColors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace mike_notepads_app.ViewModels.SettingView
 {
     public class PersonalizeViewModel : BindableBase
     {
-
+        public IEnumerable<ISwatch> Swatches { get; } = SwatchHelper.Swatches;
     }
 }
