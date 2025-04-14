783061ed0e9428358dcb12389f8e45d6c4216b2f
heyangyi <svip.hyy@qq.com>
2025-04-14 23:06:23 +0800

    完成主题设置

--- mvvm-prism-project/mike_notepads/mike_notepads_app/ViewModels/SettingView/PersonalizeViewModel.cs
+++ mvvm-prism-project/mike_notepads/mike_notepads_app/ViewModels/SettingView/PersonalizeViewModel.cs
@@ -1,14 +1,63 @@
 ﻿using MaterialDesignColors;
+using MaterialDesignColors.ColorManipulation;
+using MaterialDesignThemes.Wpf;
+using mike_notepads_app.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace mike_notepads_app.ViewModels.SettingView
 {
     public class PersonalizeViewModel : BindableBase
     {
+        private readonly PaletteHelper _paletteHelper = new();
         public IEnumerable<ISwatch> Swatches { get; } = SwatchHelper.Swatches;
+        public DelegateCommand<object> ChangeHueCommand { get; set; }
+
+
+        private bool _isDarkTheme;
+        public bool IsDarkTheme
+        {
+            get => _isDarkTheme;
+            set
+            {
+                if (SetProperty(ref _isDarkTheme, value))
+                {
+                    ModifyTheme(theme => theme.SetBaseTheme(value ? BaseTheme.Dark : BaseTheme.Light));
+                }
+            }
+        }
+
+        public PersonalizeViewModel()
+        {
+            ChangeHueCommand = new DelegateCommand<object>(ChangeHue);
+        }
+
+        private void ChangeHue(object? obj)
+        {
+            var hue = (System.Windows.Media.Color)obj!;
+
+            Theme theme = _paletteHelper.GetTheme();
+
+            theme.PrimaryLight = new ColorPair(hue.Lighten());
+            theme.PrimaryMid = new ColorPair(hue);
+            theme.PrimaryDark = new ColorPair(hue.Darken());
+
+            _paletteHelper.SetTheme(theme);
+        }
+
+        private static void ModifyTheme(Action<Theme> modificationAction)
+        {
+            var paletteHelper = new PaletteHelper();
+            Theme theme = paletteHelper.GetTheme();
+
+            modificationAction?.Invoke(theme);
+
+            paletteHelper.SetTheme(theme);
+        }
     }
 }
